3ed3aaa8055323cfc51c58ea85f9352efbe50ab0
Wei Song <popoqoqo2003@gmail.com>
2018-04-10 16:48:18 +0800

    update_v5
    update_5
    Interface
    package
    RIFile
    testing
    times
    structure

---
+++ 基礎語法學習/src/HelloWorld/HelloWorld.go
@@ -0,0 +1,8 @@
+package HelloWorld
+
+var Pack1Int int = 42
+var PackFloat = 3.14
+
+func HelloWorlds() string {
+	return "Hello World!"
+}
---
+++ 基礎語法學習/test.go
@@ -0,0 +1,18 @@
+package math
+
+import "testing"
+
+func TestAverage(t *testing.T) {
+	var v float64
+	v = Average([]float64{1, 2})
+	if v != 1.5 {
+		t.Error("Expected 1.5, got ", v)
+	}
+}
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
+}
